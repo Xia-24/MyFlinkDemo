55677dbb387b90350d875a83b9e6ed06906b74c1
胡剑 <hujian15@meituan.com>
2021-05-27 16:15:47 +0800

    Initial

---
+++ src/main/java/FlinkOrderStream.java
@@ -0,0 +1,51 @@
+import com.meituan.flink.common.config.JobConf;
+import com.meituan.flink.common.config.KafkaTopic;
+import com.meituan.flink.common.kafka.MTKafkaProducer010;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSink;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer010;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.Random;
+
+public class FlinkOrderStream {
+    private static final Logger LOG = LoggerFactory.getLogger(GenerateData.class);
+    private static final String WRITE_KAFKA_TOPIC = "app.flinkorder";
+
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStream<String> msgstream = env.addSource(new SourceFunction<String>() {
+            private Random r = new Random();
+            private static final long serialV = 1L;
+            boolean running = true;
+            @Override
+            public void run(SourceContext<String> sourceContext) throws Exception {
+                while (running){
+                    Thread.sleep(r.nextInt(1500));
+                    char catlog = (char) (65 + r.nextInt(5));
+                    sourceContext.collect(String.format("%d,%s,%d,%s,%d",System.currentTimeMillis(),String.valueOf(catlog),r.nextInt(5),FlinkRateStream.HBDM[r.nextInt(FlinkRateStream.HBDM.length)],r.nextInt(1000)));
+                }
+            }
+
+            @Override
+            public void cancel() {
+                running = false;
+            }
+        });
+//        DataStreamSink<String>
+        MTKafkaProducer010 mtKafkaProducer010 = new MTKafkaProducer010(args);
+        Map<KafkaTopic, FlinkKafkaProducer010> topic2producers = mtKafkaProducer010.getTargetTopicsToProducers();
+
+        // 添加一个Kafka Data Sink
+        for(Map.Entry<KafkaTopic,FlinkKafkaProducer010> entry:topic2producers.entrySet()){
+            msgstream.addSink(entry.getValue())
+                    .setParallelism(entry.getKey().getParallelism())
+                    .uid(WRITE_KAFKA_TOPIC).name(WRITE_KAFKA_TOPIC);
+        }
+        env.execute((new JobConf(args)).getJobName());
+    }
+}
---
+++ src/main/java/FlinkRateStream.java
@@ -0,0 +1,50 @@
+import com.meituan.flink.common.config.JobConf;
+import com.meituan.flink.common.config.KafkaTopic;
+import com.meituan.flink.common.kafka.MTKafkaProducer010;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer010;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.Random;
+
+public class FlinkRateStream {
+    public static final String[] HBDM = {"BEF","CNY","DEM","EUR","HKD","USD","ITL"};
+    private static final String WRITE_KAFKA_TOPIC = "app.flinkrate";
+    private static final Logger LOG = LoggerFactory.getLogger(GenerateData.class);
+
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStream<String> msgstream = env.addSource(new SourceFunction<String>() {
+            private Random r = new Random();
+            private static final long serial = 1L;
+            boolean running = true;
+            @Override
+            public void run(SourceContext<String> sourceContext) throws Exception {
+                while(running){
+                    Thread.sleep(r.nextInt(3)*1000);
+                    sourceContext.collect(String.format("%d,%s,%d",System.currentTimeMillis(),HBDM[r.nextInt(HBDM.length)],r.nextInt(20)));
+                }
+            }
+
+            @Override
+            public void cancel() {
+                running = false;
+            }
+        });
+        MTKafkaProducer010 mtKafkaProducer010 = new MTKafkaProducer010(args);
+        Map<KafkaTopic, FlinkKafkaProducer010> topic2producers = mtKafkaProducer010.getTargetTopicsToProducers();
+
+        // 添加一个Kafka Data Sink
+        for(Map.Entry<KafkaTopic,FlinkKafkaProducer010> entry:topic2producers.entrySet()){
+            msgstream.addSink(entry.getValue())
+                    .setParallelism(entry.getKey().getParallelism())
+                    .uid(WRITE_KAFKA_TOPIC).name(WRITE_KAFKA_TOPIC);
+        }
+        env.execute((new JobConf(args)).getJobName());
+    }
+}
